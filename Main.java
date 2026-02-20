1e2798ffc06c5a28d2bd868974ab0bc4f9abbb2b
nandkishor-vasi <nandkishorvasi@gmail.com>
2026-02-20 20:16:16 +0530

    update

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.println(calc.add(5, 3));
+        System.out.println(calc.add(6, 3));
     }
 }
